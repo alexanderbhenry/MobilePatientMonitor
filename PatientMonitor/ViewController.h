d11082977f3e2bd772fa99819aeb99727f803ba6
alexanderbhenry <alexander.henry710@gmail.com>
2014-11-11 15:59:05 -0500

    Initial Commit

---
+++ PatientMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PatientMonitor
+//
+//  Created by Alex Henry on 11/11/14.
+//  Copyright (c) 2014 Alexander Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PatientMonitor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PatientMonitor
+//
+//  Created by Alex Henry on 11/11/14.
+//  Copyright (c) 2014 Alexander Henry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
